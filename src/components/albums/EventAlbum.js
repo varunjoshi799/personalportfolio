665223ee00c253482d127f06bb72695ab8e9d586
varunjoshi799 <varunjoshi799@gmail.com>
2023-08-01 17:46:50 -0700

    updated portfolios

--- src/components/albums/EventAlbum.js
+++ src/components/albums/EventAlbum.js
@@ -5,16 +5,16 @@ import React from "react";
 
 
 // Photos
-import photo1 from "../assets/images/eventAlbum/event1.webp";
-import photo2 from "../assets/images/eventAlbum/event2.webp";
-import photo3 from "../assets/images/eventAlbum/event3.webp";
-import photo4 from "../assets/images/eventAlbum/event4.webp";
-import photo5 from "../assets/images/eventAlbum/event5.webp";
+// import photo1 from "../assets/images/eventAlbum/event1.webp";
+// import photo2 from "../assets/images/eventAlbum/event2.webp";
+// import photo3 from "../assets/images/eventAlbum/event3.webp";
+// import photo4 from "../assets/images/eventAlbum/event4.webp";
+// import photo5 from "../assets/images/eventAlbum/event5.webp";
 import photo6 from "../assets/images/eventAlbum/event6.webp";
-import photo7 from "../assets/images/eventAlbum/event7.webp";
-import photo8 from "../assets/images/eventAlbum/event8.webp";
-import photo9 from "../assets/images/eventAlbum/event9.webp";
-import photo10 from "../assets/images/eventAlbum/event10.webp";
+// import photo7 from "../assets/images/eventAlbum/event7.webp";
+// import photo8 from "../assets/images/eventAlbum/event8.webp";
+// import photo9 from "../assets/images/eventAlbum/event9.webp";
+// import photo10 from "../assets/images/eventAlbum/event10.webp";
 import photo11 from "../assets/images/eventAlbum/event11.webp";
 import photo12 from "../assets/images/eventAlbum/event12.webp";
 import photo13 from "../assets/images/eventAlbum/event13.webp";
@@ -35,16 +35,16 @@ function EventAlbum() {
             <div className="album py-5 bg-body-black">
                 <div className="container">
                     <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
-                        <Picture image={photo1} />
+                        {/* <Picture image={photo1} />
                         <Picture image={photo2} />
                         <Picture image={photo3} />
                         <Picture image={photo4} />
-                        <Picture image={photo5} />
+                        <Picture image={photo5} /> */}
                         <Picture image={photo6} />
-                        <Picture image={photo7} />
+                        {/* <Picture image={photo7} />
                         <Picture image={photo8} />
                         <Picture image={photo9} />
-                        <Picture image={photo10} />
+                        <Picture image={photo10} /> */}
                         <Picture image={photo11} />
                         <Picture image={photo12} />
                         <Picture image={photo13} />
--- src/components/albums/PortraitAlbum.js
+++ src/components/albums/PortraitAlbum.js
@@ -7,12 +7,12 @@ import BookMeAlbum from "./BookMeAlbum";
 // Photos
 import photo1 from "../assets/images/portraitAlbum/portrait1.webp";
 import photo2 from "../assets/images/portraitAlbum/portrait2.webp";
-import photo3 from "../assets/images/portraitAlbum/portrait3.webp";
+// import photo3 from "../assets/images/portraitAlbum/portrait3.webp";
 import photo4 from "../assets/images/portraitAlbum/portrait4.webp";
-import photo5 from "../assets/images/portraitAlbum/portrait5.webp";
+// import photo5 from "../assets/images/portraitAlbum/portrait5.webp";
 import photo6 from "../assets/images/portraitAlbum/portrait6.webp";
 import photo7 from "../assets/images/portraitAlbum/portrait7.webp";
-import photo8 from "../assets/images/portraitAlbum/portrait8.webp";
+// import photo8 from "../assets/images/portraitAlbum/portrait8.webp";
 import photo9 from "../assets/images/portraitAlbum/portrait9.webp";
 import photo10 from "../assets/images/portraitAlbum/portrait10.webp";
 import photo11 from "../assets/images/portraitAlbum/portrait11.webp";
@@ -36,12 +36,12 @@ function EventAlbum() {
                     <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
                         <Picture image={photo1} />
                         <Picture image={photo2} />
-                        <Picture image={photo3} />
+                        {/* <Picture image={photo3} /> */}
                         <Picture image={photo4} />
-                        <Picture image={photo5} />
+                        {/* <Picture image={photo5} /> */}
                         <Picture image={photo6} />
                         <Picture image={photo7} />
-                        <Picture image={photo8} />
+                        {/* <Picture image={photo8} /> */}
                         <Picture image={photo9} />
                         <Picture image={photo10} />
                         <Picture image={photo11} />
